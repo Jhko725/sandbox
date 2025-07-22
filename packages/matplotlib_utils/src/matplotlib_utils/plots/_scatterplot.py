a633ad34e2fb734ba6df08f9cbd2ef7a3aa02943
Joon-Hyuk Ko <jhko725@kias.re.kr>
2025-07-22 17:09:55 +0900

    Cleaned up most of the evaluation script for Lorenz63 according to the new code changes

--- packages/matplotlib_utils/src/matplotlib_utils/plots/_scatterplot.py
+++ packages/matplotlib_utils/src/matplotlib_utils/plots/_scatterplot.py
@@ -9,10 +9,14 @@ def colored_scatterplot(
     data: Sequence[Float[ArrayLike, "dim points"]],
     colors: Sequence[Float[ArrayLike, " points"]],
     figsize: tuple[int, int] = (10, 5),
+    *,
     colorbar_pad: float = 0.15,
     colorbar_width: float = 0.02,
+    vmin_percentile: float | None = 2.5,
+    vmax_percentile: float | None = 97.5,
     **scatter_kwargs,
 ):
+    ## Validate function arguments
     n_plots = len(data)
     if n_plots != len(colors):
         raise ValueError("Number of colors must match the number of data")
@@ -29,19 +33,27 @@ def colored_scatterplot(
         case _:
             raise ValueError("Dimension of the data arrays must either be 2 or 3")
 
+    if vmin_percentile is not None and "vmin" in scatter_kwargs:
+        raise ValueError("Arguments vmin_percentile and vmin are mutually exclusive")
+    if vmax_percentile is not None and "vmax" in scatter_kwargs:
+        raise ValueError("Arguments vmax_percentile and vmax are mutually exclusive")
+
+    ## Plot setup
     fig, axes = plt.subplots(1, n_plots, figsize=figsize, subplot_kw=subplot_kw)
     if not isinstance(axes, np.ndarray):
         axes = np.array([axes])
 
-    if "vmax" not in scatter_kwargs:
-        scatter_kwargs["vmax"] = max([np.max(c) for c in colors])
+    colors_all = np.concatenate(colors, axis=0)
+    if vmin_percentile is not None:
+        scatter_kwargs["vmin"] = np.nanpercentile(colors_all, vmin_percentile)
 
-    if "vmin" not in scatter_kwargs:
-        scatter_kwargs["vmin"] = min([np.min(c) for c in colors])
+    if vmax_percentile is not None:
+        scatter_kwargs["vmax"] = np.nanpercentile(colors_all, vmax_percentile)
 
     for ax, data_i, c_i in zip(axes, data, colors):
         plot = ax.scatter(*data_i, c=c_i, **scatter_kwargs)
 
+    ## Colorbar setup
     cax = fig.add_axes(
         [
             axes[-1].get_position().x1 + colorbar_pad,
