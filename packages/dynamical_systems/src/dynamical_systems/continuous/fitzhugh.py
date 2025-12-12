eb7814bd0bbe9a0fc11240ec64ef8368bc86b000
Joon-Hyuk Ko <jhko725@kias.re.kr>
2025-12-12 14:25:53 +0900

    Implemented Fitzhugh-Nagumo system and verified the limit cycle

---
+++ packages/dynamical_systems/src/dynamical_systems/continuous/fitzhugh.py
@@ -0,0 +1,20 @@
+from typing import ClassVar
+
+import jax.numpy as jnp
+
+from .ode_base import AbstractODE
+
+
+class FitzhughNagumo(AbstractODE):
+    a: float = 0.7
+    b: float = 0.8
+    c: float = 3
+    z: float = -0.4
+    dim: ClassVar[int] = 2
+
+    def rhs(self, t, u, args=None):
+        del t, args
+        x, y = u
+        dx = self.c * (y + x - x**3 / 3 + self.z)
+        dy = -(x - self.a + self.b * y) / self.c
+        return jnp.stack([dx, dy], axis=0)
